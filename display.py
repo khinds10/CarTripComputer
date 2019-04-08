562d2001c3e00b9f6ec4af162a504f1e9007ce42
Kevin Hinds <khinds10.work@gmail.com>
2019-04-08 15:41:10 -0400

    initial commit of scripts for digole display and temp logging

---
+++ display.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+# Kevin Hinds http://www.kevinhinds.com
+# License: GPL 2.0
+import datetime as dt
+import time, json, string, cgi, subprocess, json
+import settings as settings
+import Adafruit_DHT
+
+# Raspberry Pi with DHT sensor - connected to GPIO16 / Pin 36
+sensor = sensor = Adafruit_DHT.DHT11
+pin = 16
+
+# begin the loop to get the current weather for display
+while True:
+    try:
+        # get current date and time
+        date=dt.datetime.now()
+
+        # get 10 readings and average, in case the humidistat is inaccurate
+        count, readingCount, avgTemperature, avgHumidity = [ 0, 0, 0, 0 ]
+        while (count < 10):    
+            humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+            if humidity is not None and temperature is not None:
+                avgTemperature = avgTemperature + temperature
+                avgHumidity = avgHumidity + humidity
+                readingCount = readingCount + 1                
+            count = count + 1
+        avgTemperature = avgTemperature / readingCount
+        insideTemperature = int(avgTemperature * 9/5 + 32)
+        avgHumidity = avgHumidity / readingCount
+        insideHumidity = int(avgHumidity)
+        
+        # get current forecast from location
+        weatherInfo = json.loads(subprocess.check_output(['curl', settings.weatherAPIURL + settings.weatherAPIKey + '/' + str(settings.latitude) + ',' + str(settings.longitude) + '?lang=en']))
+        currentConditions = weatherInfo['currently']
+        icon = str(currentConditions['icon'])
+        apparentTemperature = str(int(currentConditions['apparentTemperature']))
+        humidity = str(int(currentConditions['humidity'] * 100))
+        windSpeed = str(int(currentConditions['windSpeed']))
+        cloudCover = str(int(currentConditions['cloudCover'] * 100))
+        precipProbability = str(int(currentConditions['precipProbability'] * 100))
+
+        # minutely conditions, limit the characters to 30 in the summary
+        minutelyConditions = weatherInfo['minutely']
+        summary = str(minutelyConditions['summary'])
+        summary = (summary[:27] + '...') if len(summary) > 29 else summary
+
+        # conditions for the day
+        dailyConditions = weatherInfo['daily']
+        dailyConditions = dailyConditions['data'][0]
+        apparentTemperatureMin = str(int(dailyConditions['apparentTemperatureMin']))
+        apparentTemperatureMax = str(int(dailyConditions['apparentTemperatureMax']))
+        
+        # clear and setup display to show basic info
+        subprocess.call(["/home/pi/CarTripComputer/digole", "clear"])
+        subprocess.call(["/home/pi/CarTripComputer/digole", "setRot90"])
+        subprocess.call(["/home/pi/CarTripComputer/digole", icon])
+        subprocess.call(["/home/pi/CarTripComputer/digole", "setFont", "18"])
+        subprocess.call(["/home/pi/CarTripComputer/digole", "setColor", "255"])
+        subprocess.call(["/home/pi/CarTripComputer/digole", "printxy_abs", "10", "178", summary])
+        subprocess.call(["/home/pi/CarTripComputer/digole", "setFont", "51"])
+        subprocess.call(["/home/pi/CarTripComputer/digole", "printxy_abs", "150", "40", date.strftime('%a, %b %d')])
+        
+        # print the min daily temp in the evening and night, print the day max temp in the morning and daytime
+        if dt.datetime.now().hour > 16 or dt.datetime.now().hour < 6:
+            subprocess.call(["/home/pi/CarTripComputer/digole", "setColor", "223"])
+            subprocess.call(["/home/pi/CarTripComputer/digole", "printxy_abs", "150", "130", "LOW\n" + apparentTemperatureMin + '*F'])
+        else:
+            subprocess.call(["/home/pi/CarTripComputer/digole", "setColor", "222"])
+            subprocess.call(["/home/pi/CarTripComputer/digole", "printxy_abs", "150", "130", "HIGH\n" + apparentTemperatureMax + '*F'])
+        
+        # show indoor / outdoor temp
+        subprocess.call(["/home/pi/CarTripComputer/digole", "setFont", "120"])
+        
+        subprocess.call(["/home/pi/CarTripComputer/digole", "setColor", "255"])
+        subprocess.call(["/home/pi/CarTripComputer/digole", "printxy_abs", "150", "85", apparentTemperature + "*F [" + humidity + "%]"])
+        
+        subprocess.call(["/home/pi/CarTripComputer/digole", "setColor", "250"])
+        subprocess.call(["/home/pi/CarTripComputer/digole", "printxy_abs", "70", "230", "IN: " + str(insideTemperature) + "* F [" + str(insideHumidity) + " %]"])
+
+        # wait 5 minutes
+        time.sleep(300)
+
+    except (Exception):
+        # Network or other issue, wait 5 minutes
+        time.sleep(300)
---
+++ settings.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# Kevin Hinds http://www.kevinhinds.com
+# License: GPL 2.0
+
+# forecast.io API key for local weather information
+weatherAPIURL = 'https://api.forecast.io/forecast/'
+weatherAPIKey = 'YOUR API KEY FOR FORECAST.IO'
+
+# optional for running the remote temp/humidity logger
+deviceLoggerAPI = 'mydevicelogger.com'
+
+# search google to get the Latitude/Longitude for your home location
+latitude = 42.4152778
+longitude = -71.1569444
